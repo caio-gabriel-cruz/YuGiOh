9e20154a0968a88d52653c81e20b0383c0d0494e
caio-gabriel-cruz <killua37pt@gmail.com>
2024-10-31 19:55:29 -0300

    3.2

--- app/src/main/java/br/com/etecia/yugioh/MainActivity.java
+++ app/src/main/java/br/com/etecia/yugioh/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.etecia.yugioh;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Card> lstCards;
 
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +31,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         lstCards = new ArrayList<>();
-            lstCards.add(new Card("drafo", R.drawable.polimeriz));
+        lstCards.add(new Card("Polimerização", R.drawable.polimeriz));
+        lstCards.add(new Card("Dragão Alado de Rá", R.drawable.alado));
+        lstCards.add(new Card("Dragão Branco de OLhos Azuis", R.drawable.bar));
+        lstCards.add(new Card("Dragão Bebê", R.drawable.bebe));
+        lstCards.add(new Card("Exodia", R.drawable.exodia));
+        lstCards.add(new Card("Exodia Necross", R.drawable.exodiapreto));
+        lstCards.add(new Card("Mago Negro", R.drawable.mago));
+        lstCards.add(new Card("Maga Negra", R.drawable.maga));
+        lstCards.add(new Card("Obelisco o Atormentador", R.drawable.obelisc));
+        lstCards.add(new Card("Mago do Tempo", R.drawable.tempo));
+        lstCards.add(new Card("Slifer o Dragão dos Céus", R.drawable.slifer));
+        lstCards.add(new Card("Kuriboh", R.drawable.kudeburro));
+        lstCards.add(new Card("Magna", R.drawable.magna));
+        //lstCards.add(new Card("Monstro que Renasce", R.drawable.renasce));
+        lstCards.add(new Card("Rei Caveira", R.drawable.caveira));
+        lstCards.add(new Card("Tyranno", R.drawable.dinossauro));
+        lstCards.add(new Card("Mundo Toon", R.drawable.fantasia));
+        lstCards.add(new Card("Gaia", R.drawable.gaia));
+        lstCards.add(new Card("Gazelle", R.drawable.gazelle));
+        lstCards.add(new Card("Maldição do Dragão", R.drawable.maldicao));
+        lstCards.add(new Card("Dragão Preto", R.drawable.preto));
 
 
         recyclerView = findViewById(R.id.idRecCard);
 
-        Adaptador adaptador = new Adaptador(getApplicationContext(),lstCards);
+        Adaptador adaptador = new Adaptador(getApplicationContext(), lstCards);
 
-        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
 
         recyclerView.hasFixedSize();
 
